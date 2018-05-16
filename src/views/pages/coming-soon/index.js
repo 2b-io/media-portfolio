d817641a6fbba01dcfee9a818e74ac313cd01121
quangtv <quang.trinh@ntq-solution.com.vn>
2018-05-16 08:36:22 +0700

    add new comming soon code

---
+++ src/views/pages/coming-soon/index.js
@@ -0,0 +1,20 @@
+import template from './view.hbs'
+
+const labels = {
+}
+
+const urls = {
+}
+
+const assets = {
+  logo_img: require('img/logo/logo_ntq.png')
+}
+
+const context = {
+  labels: labels,
+  urls: urls,
+  assets: assets
+}
+
+export default template(context)
+

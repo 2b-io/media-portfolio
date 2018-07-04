b5ce0544cf36ad97afc3a5bc066170bc5831a080
quangtv-ntq <quang.trinh@ntq-solution.com.vn>
2018-07-04 11:38:23 +0700

    get ghost data for knowledge getting-started

--- src/js/knowledge-base.js
+++ src/js/knowledge-base.js
@@ -7,21 +7,19 @@ import postTemplate from '../views/pages/knowledge-base/base-search-result.hbs'
 ghost.init({
   clientId: "ghost-frontend",
   clientSecret: "0a2a68264135"
-});
+})
 
 
 function onSuccess(data) {
-  var $result = $('#knowledge-search-wrapper');
+  var $result = $('#knowledge-search-wrapper')
   $.each(data.posts, function (i, post) {
-    $result.append(
-      postTemplate({post})
-    );
-  });
+    $result.append(postTemplate({post}))
+  })
 }
 
 
 $(document).ready(function () {
   $.get(ghost.url.api('posts', {page: 1, limit: 3})).done(onSuccess).fail(function (err){
-  console.log(err);
-  });
-});
+  console.log(err)
+  })
+})

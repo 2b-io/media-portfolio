ee0d21bf9e8b5ee98674c0c832e2b4df23118413
LongLH <d@dapps.me>
2018-10-30 21:01:10 +0700

    add parallax-js

--- src/resources/pages/home/index.js
+++ src/resources/pages/home/index.js
@@ -1,1 +1,24 @@
+// import css
 import './style'
+
+// javascript begin
+import Parallax from 'parallax-js'
+
+window.addEventListener('load', () => {
+  const scene = document.getElementById('scene')
+
+  const parallaxInstance = new Parallax(scene)
+
+  const blocks = document.querySelectorAll('.blocks')
+
+  Array.from(blocks).forEach((blocks) => {
+    for (let i = 0; i < 1; i++) {
+      const block = document.createElement('div')
+      block.className = 'block'
+
+      blocks.appendChild(block)
+      block.style.top = `${ Math.random() * (300 - 48) }px`
+      block.style.left = `${ Math.random() * (300 - 48) }px`
+    }
+  })
+})

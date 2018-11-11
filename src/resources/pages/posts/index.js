4747847b7682ee16cb5db0d9b6fafca817ad1f2a
LongLH <d@dapps.me>
2018-11-11 20:35:51 +0700

    fix scroll indicator

--- src/resources/pages/posts/index.js
+++ src/resources/pages/posts/index.js
@@ -2,13 +2,12 @@ import { throttle } from 'throttle-debounce'
 
 const updateScrollIndicator = () => {
   const vpHeight = window.innerHeight
-  const scrollTop = document.body.parentElement.scrollTop
-  const scrollHeight = document.body.parentElement.scrollHeight
+  const scrollTop = document.body.scrollTop || document.body.parentElement.scrollTop || 0
+  const scrollHeight = document.body.scrollHeight || document.body.parentElement.scrollHeight || vpHeight
 
   const scrollProgress = scrollTop / (scrollHeight - vpHeight) * 100
 
   const scrollIndicator = document.getElementById('scroll-indicator')
-
 
   if (scrollIndicator) {
     scrollIndicator.style.width = `${ scrollProgress }%`

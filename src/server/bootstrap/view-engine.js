a647ae935edc8c681c7eb7f825b6de2ce6b50155
LongLH <d@dapps.me>
2018-11-14 11:20:19 +0700

    add html minifier

--- src/server/bootstrap/view-engine.js
+++ src/server/bootstrap/view-engine.js
@@ -1,4 +1,5 @@
 import ect from 'ect'
+import { minify } from 'html-minifier'
 import path from 'path'
 
 import config from '../infrastructure/config'
@@ -14,5 +15,22 @@ export default (app) => {
 
   app.set('view engine', 'ect')
   app.set('views', viewDir)
-  app.engine('ect', engine.render)
+  app.engine('ect', (template, data, cb) => {
+    engine.render(template, data, (error, html) => {
+      if (error) {
+        return cb(error)
+      }
+
+      const minified = minify(html, {
+        collapseWhitespace: true,
+        preserveLineBreaks: true,
+        sortAttributes: true,
+        sortClassName: true
+      })
+
+      console.log(`MINIFIED HTML... ${ html.length } -> ${ minified.length }`)
+
+      cb(null, minified)
+    })
+  })
 }
